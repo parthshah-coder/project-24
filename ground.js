3f7466ee8210399cfd8694606f30dc3c13e84e72
parthshah-coder <72604239+parthshah-coder@users.noreply.github.com>
2020-11-04 15:08:39 +0530

    Add files via upload

--- ground.js
+++ ground.js
@@ -9,7 +9,7 @@ class Ground{
         World.add(world,this.body);
     }
     display(){
-        fill("red");
+        fill("yellow");
         rectMode(CENTER);
         rect(this.body.position.x,this.body.position.y,this.width,this.height)
     }
